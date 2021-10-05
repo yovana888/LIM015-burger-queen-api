c8ccab0c478e26aa79a92480ddc2395eb92125db
Alisson <rocio.cruz@unmsm.edu.pe>
2021-10-05 13:14:33 -0500

    config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 exports.port = process.argv[2] || process.env.PORT || 8080;
-exports.dbUrl = process.env.DB_URL || 'mongodb+srv://bqconnection:Zf5WyPHcwD0Bl74k@bq-database.bve0q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+exports.dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/test';
 exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
 exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost';
-exports.adminPassword = process.env.ADMIN_PASSWORD || 'changeme';
+exports.adminPassword = process.env.ADMIN_PASSWORD || 'password$123';
+console.info(process.env);
