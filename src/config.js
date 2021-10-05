90fc6456cafd7f0031c7a777fb3151efe385e700
Alisson <rocio.cruz@unmsm.edu.pe>
2021-10-05 13:35:50 -0500

    variables de entorno

--- src/config.js
+++ src/config.js
@@ -2,5 +2,4 @@ exports.port = process.argv[2] || process.env.PORT || 8080;
 exports.dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/test';
 exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
 exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost';
-exports.adminPassword = process.env.ADMIN_PASSWORD || 'password$123';
-console.info(process.env);
+exports.adminPassword = process.env.ADMIN_PASSWORD || 'changeme';
