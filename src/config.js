217cb0d3f26df10aab9d3201cf1d355dc568d994
Alisson <rocio.cruz@unmsm.edu.pe>
2021-10-05 11:20:21 -0500

    mlab database

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 exports.port = process.argv[2] || process.env.PORT || 8080;
-exports.dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/test';
+exports.dbUrl = process.env.DB_URL || 'mongodb+srv://bqconnection:Zf5WyPHcwD0Bl74k@bq-database.bve0q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
 exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost';
 exports.adminPassword = process.env.ADMIN_PASSWORD || 'changeme';
