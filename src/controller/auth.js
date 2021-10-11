98ff716d35df140e83cba800fefe0d9e0ae50e1e
Alisson <rocio.cruz@unmsm.edu.pe>
2021-10-10 21:42:20 -0500

    cambiando protocolo https por ssh

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -1,19 +1,23 @@
 const { comparePassword, generateJWT, getUserByEmail } = require('../services/users');
 
 // Autenticar
-module.exports.auth = async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) return res.status(400).json({ message: 'Email or password not found' });
+module.exports.auth = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) return res.status(400).json({ message: 'Email or password not found' });
 
-  // si no existe el usuario
-  const userFound = await getUserByEmail(email);
-  if (!userFound) return res.status(404).json({ message: "User doesn't exists" });
+    // si no existe el usuario
+    const userFound = await getUserByEmail(email);
+    if (!userFound) return res.status(404).json({ message: "User doesn't exists" });
 
-  // se comparan las contraseñas
-  const existsPassword = await comparePassword(password, userFound.password);
-  if (!existsPassword) return res.status(404).json({ message: 'Invalid password' });
+    // se comparan las contraseñas
+    const existsPassword = await comparePassword(password, userFound.password);
+    if (!existsPassword) return res.status(404).json({ message: 'Invalid password' });
 
-  // TODO: autenticar a la usuarix
-  const token = await generateJWT(userFound._id);
-  return res.json({ token });
+    // TODO: autenticar a la usuarix
+    const token = await generateJWT(userFound._id);
+    return res.json({ token });
+  } catch (err) {
+    return next(err);
+  }
 };
