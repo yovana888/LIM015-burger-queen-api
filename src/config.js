2ed01669aa862b72e2ad7b029e498400348e66cf
yovanav.delfosti <yovana.velasquez@delfosti.com>
2023-06-13 13:11:34 -0500

    change connect db

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 exports.port = process.argv[2] || process.env.PORT || 8080;
-exports.dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/test';
+exports.dbUrl = process.env.DB_URL || 'mongodb+srv://root:bce15YSMZLq0q5S4@cluster0.ggi2l.mongodb.net/burgerdb';
 exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
 exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost';
 exports.adminPassword = process.env.ADMIN_PASSWORD || 'admin123';
